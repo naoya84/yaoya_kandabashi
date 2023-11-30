e27a864f135442fbe1b101be5be415e70496cf62
YokotaYusuke <tarut8166@gmail.com>
2023-11-30 16:44:46 +0900

    login追加

--- front/src/component/Home.jsx
+++ front/src/component/Home.jsx
@@ -6,11 +6,27 @@ import '../assets/style/Home.css';
 export default function Home() {
   //   const { team } = props;
 
+  const handleBtnClick = () => {
+    console.log('ボタンクリックしました！！！');
+  };
+
   useEffect(() => {}, []);
 
   return (
     <>
-      <img src={topImg} alt="マップの画像" />
+      <div className="home-container">
+        <div className="home-login">
+          <h1 className="login-title">yaoya へようこそ</h1>
+          <p className="login-text">ユーザー名</p>
+          <input className="login-input" type="text" />
+          <p className="login-text">パスワード</p>
+          <input className="login-input" type="text" />
+          <button className="login-btn" onClick={handleBtnClick}>
+            ログイン
+          </button>
+        </div>
+        <img src={topImg} alt="マップの画像" />
+      </div>
     </>
   );
 }
