2634d4f08df16ee215a12043b6e1f87b85b9746a
shogo <shogo_inoue@mail.toyota.co.jp>
2023-12-02 03:19:24 +0900

    fix db table column & fix seed sample data

---
+++ back/db/migrations/20231201160722_fix_shopping_list_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table('shopping_list', function (table) {
+    table.string('unit', 32).notNullable;
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table('shopping_list', function (table) {
+    table.dropColumn('unit');
+  });
+};
--- back/db/seeds/003-shopping_list.js
+++ back/db/seeds/003-shopping_list.js
@@ -1,22 +1,54 @@
 exports.seed = function (knex) {
   // ダミーデータの生成
-  let seedData = [];
-  for (let i = 1; i <= 50; i++) {
-    seedData.push({
-      id: i,
-      userId: Math.random() < 0.5 ? 1 : 2, // userIdは1または2
-      storeId: Math.random() < 0.5 ? 1 : 2, // storeIdも1または2
-      productName: `商品${i}`,
-      flag: Math.random() < 0.5, // 約50%の確率でtrueまたはfalse
+  const shoppingListSample = [
+    {
+      id: 1,
+      userId: 1,
+      storeId: 2,
+      productName: 'かぼちゃ',
+      piece: 1,
+      unit: '1/2個',
+      flag: true,
       time: new Date(), // 現在のタイムスタンプ
-    });
-  }
+    },
+    {
+      id: 2,
+      userId: 1,
+      storeId: 3,
+      productName: 'りんご',
+      piece: 3,
+      unit: '個',
+      flag: true,
+      time: new Date(), // 現在のタイムスタンプ
+    },
+    {
+      id: 3,
+      userId: 1,
+      storeId: 2,
+      productName: '牛肉',
+      piece: 300,
+      unit: 'g',
+      flag: true,
+      time: new Date(), // 現在のタイムスタンプ
+    },
+  ];
+
+  // for (let i = 1; i <= 50; i++) {
+  //   seedData.push({
+  //     id: i,
+  //     userId: Math.random() < 0.5 ? 1 : 2, // userIdは1または2
+  //     storeId: Math.random() < 0.5 ? 1 : 2, // storeIdも1または2
+  //     productName: `商品${i}`,
+  //     flag: Math.random() < 0.5, // 約50%の確率でtrueまたはfalse
+  //     time: new Date(), // 現在のタイムスタンプ
+  //   });
+  // }
 
   // テーブル内の既存データを削除
-  return knex("shopping_list")
+  return knex('shopping_list')
     .del()
     .then(function () {
       // 新しいシードデータを挿入
-      return knex("shopping_list").insert(seedData);
+      return knex('shopping_list').insert(shoppingListSample);
     });
 };
