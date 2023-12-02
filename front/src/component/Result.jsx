b0e855c40e7b8ef9cdd4619a825257cc0b7eb637
YokotaYusuke <tarut8166@gmail.com>
2023-12-02 16:50:47 +0900

    Resultに遷移した時、ResultStoreに遷移した時の実装

--- front/src/component/Result.jsx
+++ front/src/component/Result.jsx
@@ -6,20 +6,78 @@ import storeImg from '../assets/image/store.jpg';
 import shoppingImg from '../assets/image/shopping.jpg';
 
 export default function Result() {
-  //   const { team } = props;
-  const [store, setStore] = useState([]);
+  // const [store, setStore] = useState([]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`/api/customers/1/result/store`) //後でユーザーIDに書き換え
+  //     .then((response) => {
+  //       setStore(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       alert('取得に失敗しました');
+  //     });
+  // }, []);
+
+  // ------------- new-start --------------------
+
+  const [storeProduct, setStoreProduct] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`/api/customers/1/result/store`) //後でユーザーIDに書き換え
-      .then((response) => {
-        setStore(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert('取得に失敗しました');
-      });
+    const arr = [
+      { id: 1, storeId: 1, productName: 'ヨーグルト', piece: 5, unit: '個', flag: false, storeName: 'プライムツリー赤池' },
+      { id: 2, storeId: 2, productName: 'かぼちゃ', piece: 1, unit: '個', flag: false, storeName: 'ワークマン' },
+      { id: 3, storeId: 3, productName: '豚肉', piece: 10, unit: '個', flag: false, storeName: 'セブンイレブン' },
+      { id: 4, storeId: 3, productName: 'あじ', piece: 2, unit: '個', flag: false, storeName: 'セブンイレブン' },
+      { id: 5, storeId: 3, productName: '塩', piece: 3, unit: '個', flag: false, storeName: 'セブンイレブン' },
+    ];
+    setStoreProduct(arr);
   }, []);
+
+  const product = storeProduct.map((el, index) => {
+    return (
+      <label key={index}>
+        <input type="checkbox" />
+        {el.productName} ({el.piece}
+        {el.unit})
+      </label>
+    );
+  });
+
+  const store = () => {
+    const storeArray = storeProduct.map((el) => {
+      return JSON.stringify({ storeId: el.storeId, storeName: el.storeName });
+    });
+    const uniqStoreArray = Array.from(new Set([...storeArray])).map((el) => JSON.parse(el));
+
+    // console.log(uniqStoreArray);
+
+    return uniqStoreArray.map((el, index) => {
+      return (
+        <React.Fragment key={index}>
+          <Link to={`/result/store/${el.storeId}`} state={{ storeName: el.storeName }} className="Link">
+            {el.storeName}
+          </Link>
+        </React.Fragment>
+      );
+    });
+  };
+
+  const handleFlagChangeClick = () => {
+    console.log('クリックした！');
+    // チェックボックスにチェック入っているか見にいく
+    // ~~~~~~処理~~~~~
+
+    // 配列で取得する。
+    const idArr = [1, 2];
+
+    // idをPATCHリクエストのbodyで送信
+
+    // バックエンドでidと一致するデータのflagをtrueにする
+
+    // バックエンドはshopping_listテーブルのデータ全て返す。
+  };
 
   return (
     <>
@@ -27,45 +85,14 @@ export default function Result() {
       <div className="result-container">
         <div className="shopping-list" style={{ backgroundImage: `url(${shoppingImg})` }}>
           <h1>買い物リスト</h1>
-          <div className="shopping-box">
-            <label>
-              <input type="checkbox" />
-              りんご
-            </label>
-            <label>
-              <input type="checkbox" />
-              バナナ
-            </label>
-            <label>
-              <input type="checkbox" />
-              レモン
-            </label>
-            <label>
-              <input type="checkbox" />
-              りんご
-            </label>
-            <label>
-              <input type="checkbox" />
-              バナナ
-            </label>
-            <label>
-              <input type="checkbox" />
-              レモン
-            </label>
-          </div>
+          <div className="shopping-box">{product}</div>
+          <button className="shopping-btn" onClick={handleFlagChangeClick}>
+            選択した商品を購入済みに変更する
+          </button>
         </div>
         <div className="store-list" style={{ backgroundImage: `url(${storeImg})` }}>
           <h1>ショップの提案</h1>
-          <div className="store-box">
-            {store.map((obj, index) => (
-              <React.Fragment key={index}>
-                {/* <Link to={{ pathname: `/result/store/${obj.id}`, state: { store: 'obj.storeName' } }} className="Link"> */}
-                <Link to={`/result/store/${obj.id}`} state={{ storeName: obj.storeName }} className="Link">
-                  {obj.storeName}
-                </Link>
-              </React.Fragment>
-            ))}
-          </div>
+          <div className="store-box">{store()}</div>
         </div>
       </div>
     </>
