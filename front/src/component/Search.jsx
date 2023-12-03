8513c19fd58b4bd4047843b70ca9c2bb4b343672
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-03 23:32:49 +0900

    deploy01

--- front/src/component/Search.jsx
+++ front/src/component/Search.jsx
@@ -67,7 +67,7 @@ export default function Search() {
 
 		try {
 			const url = import.meta.env.VITE_DEVELOPMENT_BACKEND_URL || "https://yaoya-lenzzzz.onrender.com";
-			const response = await fetch(url + `/api/customers/1/shopping_list`, {
+			const response = await fetch(url + `/api/customers/${userId}/shopping_list`, {
 				method: 'POST',
 				credentials: 'include', // クロスオリジンリクエストでのクッキー送信を許可
 				headers: {
